656ce93543c9065e73e3004a08f9475ceab55b65
Nick Lanese <nlanese21@gmail.com>
2020-07-08 06:00:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(number)
+  counter = 0
+  i = 0
+  j = 0
+  while i < number
+    while j < number
+      if ((i * j) == number)
+        counter = counter + 1
+      end
+      j = j + 1
+    end
+    i = i + 1
+  end
+  if (counter >= 3)
+    return false
+  else
+    return true
+  end
+end
+
+      
